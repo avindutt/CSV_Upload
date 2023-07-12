cdf4aee153f36183effb5fa6e4dea7f1c56f27be
avindutt <avindutt2369@gmail.com>
2023-07-13 00:35:17 +0530

    debugging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 8000;
+// const port = 8000;
 const app = express();
 const path = require('path');
 const csv = require('csv-parser');
@@ -19,9 +19,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', require('./routes'));
 
-app.listen(port, function(err){
+const PORT = process.env.PORT;
+
+app.listen(PORT, function(err){
     if(err){
         console.log('Error in connecting to server', err);
     }
-    console.log('Successfully connected to port : ', port);
+    console.log('Successfully connected to port : ', PORT);
 })
